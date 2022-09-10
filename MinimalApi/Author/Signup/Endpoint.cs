abd25f048b073b3c1ddb10bd064687b7c2f4aa13
Dmitry Tarakanov <mail.dimm@gmail.com>
2022-09-10 22:38:41 +0300

    new first endpoint

---
+++ MinimalApi/Author/Signup/Endpoint.cs
@@ -0,0 +1,19 @@
+namespace Author.Signup;
+
+public class Endpoint : Endpoint<Request, Response, Mapper>
+{
+    public override void Configure()
+    {
+        Verbs(Http.POST);
+        Routes("/author/signup");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(Request r, CancellationToken c)
+    {
+        await SendAsync(new Response()
+        {
+            //пока будет пусто
+        });
+    }
+}
